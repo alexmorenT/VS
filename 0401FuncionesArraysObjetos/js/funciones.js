c13bbea6fb98db3a7052d4285a197a0aabf63e96
alexmorenT <amoretroco@gmail.com>
2025-11-04 12:13:44 +0100

    Nuevos ejercicios de funciones

--- 0401FuncionesArraysObjetos/js/funciones.js
+++ 0401FuncionesArraysObjetos/js/funciones.js
@@ -62,3 +62,44 @@ function palindromo(frase) {
     return igual;
 
 }
+
+// Ejercicio 7 
+
+function fechaFormateada(fecha) {
+
+    document.getElementById("mensaje").innerHTML += "Fecha de hoy: " + "<strong>" + fecha.toLocaleDateString() + "</strong>" + "<br>"
+
+}
+
+function diaSemana(fecha) {
+
+    let diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+
+    document.getElementById("mensaje").innerHTML += "Día de la semana: " + "<strong>" + diasSemana[fecha.getDay()] + "</strong>" + "<br>"
+}
+
+function mes(fecha) {
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octube", "Noviembre", "Diciembre"]
+    document.getElementById("mensaje").innerHTML += "Mes: " + "<strong>" + meses[fecha.getMonth()] + "</strong>" + "<br>"
+}
+
+// Ejercicio 8
+
+function cifrar(palabra) {
+    let abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let abecedarioR = [...abecedario].reverse()
+
+    let p = palabra.trim().toLowerCase()
+    let nuevaP = ""
+
+    for (let index = 0; index < p.length; index++) {
+        if (abecedario.includes(p.charAt(index))) {
+            nuevaP += abecedarioR[abecedario.indexOf(p.charAt(index))].toUpperCase()
+        }
+    }
+
+    document.getElementById("mensaje").innerHTML = "Su palabra cifrada es: " + "<strong>" + nuevaP + "</strong>"
+
+
+
+}
